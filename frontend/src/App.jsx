cbee4ecad5e6542d215fa565a8487d2413f74dbc
Aqib Nawaz <aqibnawazcs@gmail.com>
2024-03-18 16:46:07 +0500

    TopBar and header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 import TopBar from "./components/TopBar";
 
 export default function App() {
   return (
     <div className="w-full">
       <TopBar />
+      <Header />
     </div>
   );
 }
--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import { FaChevronDown } from "react-icons/fa6";
 
 function TopBar() {
   return (
-    <div className="bg-black text-white py-1">
-      <div className="w[90%] w-full flex justify-center align-middle">
-        <p className="text-[14px]">
-          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-        </p>
-        <a className="outline-none underline mx-3 font-semibold text-[14px] cursor-pointer">
-          Show Now
-        </a>
-      </div>
-      <div>
-        <p>English icon</p>
+    <div className="bg-black text-white py-1 w-full">
+      <div className="w-[90%] flex justify-end">
+        <div className=" w-[70%] flex justify-between items-center">
+          <div className="flex justify-center ">
+            <p className="text-[14px]">
+              Summer Sale For All Swim Suits And Free Express Delivery - OFF
+              50%!
+            </p>
+            <a className="outline-none underline mx-3 font-semibold text-[14px] cursor-pointer">
+              Show Now
+            </a>
+          </div>
+          <div className="flex justify-between items-center cursor-pointer">
+            <p className="text-[14px]">English</p>
+            <FaChevronDown size={"14px"} className="ms-2" />
+          </div>
+        </div>
       </div>
     </div>
   );
