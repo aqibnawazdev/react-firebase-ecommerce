28fa75b8730f7b285070725078906689a03496f5
Aqib Nawaz <aqibnawazcs@gmail.com>
2024-03-20 11:39:57 +0500

    Auth-Register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,20 @@ import HeroSlider from "./components/HeroSlider";
 import ProductCard from "./components/ProductCard";
 import SectionTitle from "./components/SectionCatTitle";
 import TopBar from "./components/TopBar";
-import CategorySection from "./Sections/CategorySection";
-import HeroSection from "./Sections/HeroSection";
-import TodayFlashSaleSection from "./Sections/TodayFlashSaleSection";
+import AuthRegister from "./pages/AuthRegister";
+// import CategorySection from "./sections/CategorySection";
+// import HeroSection from "./sections/HeroSection";
+// import TodayFlashSaleSection from "./sections/TodayFlashSaleSection";
 
 export default function App() {
   return (
     <div className="w-full">
       <TopBar />
       <Header />
-      <HeroSection />
+      {/* <HeroSection />
       <TodayFlashSaleSection />
-      <CategorySection />
+      <CategorySection /> */}
+      <AuthRegister />
     </div>
   );
 }
---
+++ frontend/src/pages/AuthRegister.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+
+function AuthRegister() {
+  return (
+    <div className="w-full mt-5 md:mt-10 flex justify-center md:block">
+      <div className="w-[90%] object-contain flex md:justify-between justify-center items-center shadow md:shadow-none py-5 md:py-3  gap-10">
+        <div className="left w-[50%] hidden md:block">
+          <img
+            src="./img/auth-screen-img.png"
+            alt=""
+            height={200}
+            className="img overflow-hidden"
+          />
+        </div>
+        <div className="md:w-[40%] w-[90%]">
+          <h1 className="text-xl sm:text-3xl font-semibold">
+            Create an Account
+          </h1>
+          <span className="text-xs">Enter your details below</span>
+          <form action="" className="flex flex-col gap-4 mt-5 ">
+            <input
+              type="text"
+              placeholder="Name"
+              className="p-1 border-b outline-none border-black sm:w-[80%] w-full"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="p-1 border-b outline-none border-black sm:w-[80%] w-full"
+            />
+            <input
+              type="number"
+              placeholder="Phone number"
+              className="p-1 border-b outline-none border-black sm:w-[80%] w-full"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="p-1 border-b outline-none border-black sm:w-[80%] w-full"
+            />
+            <input
+              type="file"
+              className="p-1  outline-none sm:w-[80%] w-full"
+            />
+            <button className="p-1 bg-red-500 text-white text-md sm:w-[80%] w-full mt-10">
+              Create account
+            </button>
+            <button className="p-1 bg-white sm:w-[80%] w-full flex justify-center gap-2 items-center shadow">
+              <FcGoogle size={25} />
+              <span className="text-md">Singup with Google</span>
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AuthRegister;
