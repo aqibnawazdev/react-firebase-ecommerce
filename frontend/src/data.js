44dbe0912aab19731d13833c158769ed2dc80d17
Aqib Nawaz <aqibnawazcs@gmail.com>
2024-03-21 12:00:31 +0500

    Cart page

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,23 +1,31 @@
 export const product = [
     {
+        id: 1,
+        desc: "PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.",
         title: "HAVIT HV-G92 Gamepad",
         price: 120,
         originalPrice: 160,
         img: "/img/gamepad.png",
     },
     {
+        id: 2,
+        desc: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate repudiandae aliquid quia nam! Quae asperiores ipsam ullam rerum,",
         title: "AK-900 Wired Keyboard",
         price: 960,
         originalPrice: 1130,
         img: "/img/keyboard.png",
     },
     {
+        id: 3,
+        desc: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate repudiandae aliquid quia nam! Quae asperiores ipsam ullam rerum,",
         title: "IPS LCD Gaming Monitor",
         price: 370,
         originalPrice: 470,
         img: "/img/display.png",
     },
     {
+        id: 4,
+        desc: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate repudiandae aliquid quia nam! Quae asperiores ipsam ullam rerum,",
         title: "S-Series Comfort Chair",
         price: 378,
         originalPrice: 450,
---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { product } from "../data";
+
+function Cart() {
+  return (
+    <div className="w-full flex flex-col justify-center items-center mt-10">
+      <table className="table-auto w-[90%]  overflow-x-auto text-left ">
+        <thead className=" shadow-sm">
+          <tr className=" bg-gray-100 m-5">
+            <th scope="col" className="py-3">
+              Product
+            </th>
+            <th scope="col" className="py-3">
+              Price
+            </th>
+            <th scope="col" className="py-3">
+              Quantity
+            </th>
+            <th scope="col" className="py-3">
+              Subtotal
+            </th>
+          </tr>
+        </thead>
+        <tbody className=" mt-6">
+          {product.map((p) => (
+            <tr key={p.id} className="items-start shadow-sm">
+              <td scope="col" className="flex items-center py-2 gap-2 relative">
+                <button className="bg-red-500 cart-prod-remove-btn absolute top-1 left-[-10px] rounded-full w-4 h-4 text-[10px] text-white">
+                  X
+                </button>
+                <img src={p.img} alt="" width={50} />
+                <span className="text-xs sm:text-sm">{p.title}</span>
+              </td>
+              <td scope="col" className=" py-2">
+                ${p.price}
+              </td>
+              <td scope="col" className="py-2">
+                <div className=" w-[20px] h-full">
+                  <input
+                    className="w-[45px] py-1 ps-2 border"
+                    type="number"
+                    defaultValue={1}
+                  />
+                </div>
+              </td>
+              <td scope="col" className="py-2">
+                ${p.price}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="actions flex justify-between w-[90%] mt-10 ">
+        <button className="w-50 py-2 px-4 border border-black bg-white">
+          Return to Shop
+        </button>
+        <button className="w-50 py-2 px-4 border border-black bg-white">
+          Update Cart
+        </button>
+      </div>
+      <div className="flex w-[90%] justify-between items-start mt-10 mb-10">
+        <div className="w-[60%] flex gap-3">
+          <input type="text" className="py-1 px-3 border border-black" />
+          <button className="bg-red-500 py-1 px-3 text-white">
+            Apply coupon
+          </button>
+        </div>
+        <div className="shipping-details flex flex-col gap-3 w-[30%] border border-black p-5">
+          <h2>Cart Total</h2>
+          <div className="border-b border-b-black flex justify-between">
+            <span className="text-sm">Subtotal: </span>
+            <span className="text-sm">${"1080"}</span>
+          </div>
+          <div className="border-b  border-b-black flex justify-between">
+            <span className="text-sm">Shipping: </span>
+            <span className="text-sm">{"Free"}</span>
+          </div>
+          <div className=" flex justify-between mt-2">
+            <span className="text-sm font-semibold">Total: </span>
+            <span className="text-sm font-semibold">${1800}</span>
+          </div>
+          <div className="self-center">
+            <button className="bg-red-500 py-1 px-2 rounded text-white text-sm">
+              Proccess to checkout
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
